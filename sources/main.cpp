dcb5669e2f5d733bc74aa0b6bb95b01b7837c0fa
Corentin DROUET <cdrouet@e1r4p21.42.fr>
2017-02-07 10:07:42 +0100

    Wording

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/23 15:27:42 by cdrouet           #+#    #+#             */
-/*   Updated: 2017/02/06 14:36:39 by cdrouet          ###   ########.fr       */
+/*   Updated: 2017/02/07 10:06:45 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ int main( int argc, char **argv )
       ExecFile::ExecFile().useFile(argv[1]);
     else {
       std::cout << "0 or 1 arguments needed" << std::endl;
+      std::cout << "0 argument for standard input" << std::endl;
+      std::cout << "1 argument for input from a file (should be a valid file)"
+        << std::endl;
       return 1;
     }
   } catch (std::exception & e) {
